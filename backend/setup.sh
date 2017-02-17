81a5208a3f1811ef68d1dff5991b21c7657e38d5
Anthony Dellinger <18DellingerA@smsd.local>
2017-02-17 14:57:21 -0500

    fixed install script

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,13 +2,13 @@
 touch DEV;
 cat "fjdkfjsdhkdfj&*^&^*)()" > SECRET;
 
-sudo apt-get install python3-pip python-dev;
+apt-get install python3-pip python-dev;
 pip3 install -r requirements.txt;
 
 cd ~
 git clone https://www.github.com/datalogai/recurrentshop.git
 cd recurrentshop
-python setup.py install
+python3 setup.py install
 
 sudo pip3 install git+https://github.com/farizrahman4u/seq2seq.git
 
