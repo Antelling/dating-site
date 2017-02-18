f1fd4f0c784db3a8ade9bde483f2f37856faf299
Anthony Dellinger <18DellingerA@smsd.local>
2017-02-17 20:31:53 -0500

    added untested personality quiz grading code

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration', include('rest_auth.registration.urls')),
-    url(r'^test/', include('personality_test.urls')),
+    url(r'^personality_test/', include('personality_test.urls')),
 ]
 
--- backend/personality_test/views.py
+++ backend/personality_test/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render
-
+import math
 from django.http import HttpResponse
+
+
+def reverse(num):
+    return 6 - num
+
+
+def extract(num):
+    return [False if num > 0 else True, num if num > 0 else -1 * num]
+
+
 # Create your views here.
 def five_parts(request):
-    return HttpResponse(request.user.username)
+    map = [
+        1, -2, 3, -4, 5,
+        -1, 2, -3, 4, -5,
+        1, -2, 3, -4, 5,
+        -1, 2, -3, 4, -5,
+        1, -2, 3, -4, 5,
+        -1, 2, -3, 4, -5,
+        1, -2, 3, -4, 5,
+        -1, 2, -3, -4, 5, #and the pattern dies
+        1, 2, 3, -4, 5,
+        -1, 2, 3, -4, 5
+    ]
+    map = {
+        "1": "extraversion",
+        "2": "agreeableness",
+        "3": "conscientiousness",
+        "4": "emotional_stability",
+        "5": "imagination"
+    }
+    total = {
+        "extraversion": 0,
+        "agreeableness": 0,
+        "conscientiousness": 0,
+        "emotional_stability": 0,
+        "imagination": 0
+    }
+    if request.POST.response:
+        for i, question in enumerate(request.POST.response):
+            negative, key = extract(map[i])
+            if negative:
+                question = reverse(question)
+            key = map[key]
+            total[key] += question
+        return HttpResponse(str(total))
+    else:
+        return HttpResponse(request.user.username)
