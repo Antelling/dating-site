13b774e5286759f31d075ffd59d4b2b2383e223c
Anthony Dellinger <18DellingerA@smsd.local>
2017-02-19 19:20:14 -0500

    added /user_info page

--- backend/static_pages/views.py
+++ backend/static_pages/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.contrib.staticfiles.views import serve
+from django.http import HttpResponse
+
+import json
+
 
 # Create your views here.
 def index(request):
-    print(request.user)
-    print(request.user.is_authenticated)
-    print(request.user.username)
     if request.user.is_authenticated():
         return serve(request, "dashboard.html")
     else:
         return serve(request, "index.html")
+
+
+def info(request):
+    return HttpResponse(json.dumps({
+        "is_staff": request.user.is_staff,
+        "is_superuser": request.user.is_superuser,
+        "username": request.user.username,
+        "is_active": request.user.is_active,
+        "email": request.user.email
+    }))
