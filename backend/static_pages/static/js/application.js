1e216a1e0a78d75d3394cd54e73831c91bcd57f7
Franklin Harding <franklin@fharding.localdomain>
2017-02-19 16:25:32 -0800

    Fixed /dashboard to use the official API endpoint /user_info instead of /personality_test temporary one.

--- backend/static_pages/static/js/application.js
+++ backend/static_pages/static/js/application.js
@@ -3,11 +3,13 @@ window.onload = () => {
     el: "#application",
     data: {
       customAxios: axios.create({
+        // transform request injects the csrfmiddlwaretoken to every form and url encodes.
         transformRequest:function(data) {var str = [];data.csrfmiddlewaretoken=docCookies.getItem("XSRF-TOKEN");for(var p in data) {if (data.hasOwnProperty(p) && data[p]) {str.push(encodeURIComponent(p) + "=" + encodeURIComponent(data[p]));}}return str.join("&");},
         headers: { "Content-Type" : "application/x-www-form-urlencoded" }
       }),
       user: {
-        username: ""
+        username: "",
+        email: ""
       },
       baseUrl: "http://localhost/"
     },
@@ -16,8 +18,9 @@ window.onload = () => {
     },
     methods: {
       getUserInformation: function() {
-        this.customAxios.post(this.baseUrl + "personality_test/", {}).then(response => {
-          this.user.username = response.data;
+        this.customAxios.post(this.baseUrl + "user_info/", {}).then(response => {
+          this.user.username = response.data.username;
+          this.user.email = response.data.email;
         })
       },
       logOut: function() {
